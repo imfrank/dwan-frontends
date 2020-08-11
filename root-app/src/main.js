4b01f7b06873d4fe473847755e85d3fb2a4e8ad2
frank <imfrank.qin@foxmail.com>
2020-08-11 19:46:41 +0800

    qiankun impl

--- root-app/src/main.js
+++ root-app/src/main.js
@@ -4,20 +4,37 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import { registerMicroApps, start } from 'qiankun';
+import { registerMicroApps, runAfterFirstMounted, setDefaultMountApp, start, initGlobalState } from 'qiankun';
+
+import render from '../render/VueRender';
+
+render({ loading: true });
+
+const loader = loading => render({ loading });
+
 
 registerMicroApps([ 
-  {
-    name: 'vue app',
-    entry: { scripts: ['//localhost:7100/main.js'] },
-    container: '#app',
-    activeRule: '/yourActiveRule2',
-  },
+  { name: 'product', entry: '//localhost:7101',   container: '#subapp-viewport',activeRule:'product',loader },
 ]);
 
 
 Vue.config.productionTip = false
 
+const { onGlobalStateChange, setGlobalState } = initGlobalState({
+  user: 'qiankun',
+});
+
+onGlobalStateChange((value, prev) => console.log('[onGlobalStateChange - master]:', value, prev));
+
+setGlobalState({
+  ignore: 'master',
+  user: {
+    name: 'master',
+  },
+});
+
+
+//setDefaultMountApp('galaxy-product'); //默认页面
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
