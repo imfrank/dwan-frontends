eeb82b942481c4222d878abbce8ed76d9515284e
frank <imfrank.qin@foxmail.com>
2020-08-12 20:09:30 +0800

    更改vue版本

--- root-app/src/main.js
+++ root-app/src/main.js
@@ -1,45 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
-import { registerMicroApps, runAfterFirstMounted, setDefaultMountApp, start, initGlobalState } from 'qiankun';
-
-import render from '../render/VueRender';
-
-render({ loading: true });
-
-const loader = loading => render({ loading });
-
-
-registerMicroApps([ 
-  { name: 'product', entry: '//localhost:7101',   container: '#subapp-viewport',activeRule:'/product',loader },
-]);
-
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
-const { onGlobalStateChange, setGlobalState } = initGlobalState({
-  user: 'qiankun',
-});
-
-onGlobalStateChange((value, prev) => console.log('[onGlobalStateChange - master]:', value, prev));
-
-setGlobalState({
-  ignore: 'master',
-  user: {
-    name: 'master',
-  },
-});
-
-
-//setDefaultMountApp('galaxy-product'); //默认页面
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
-start();
+  render: h => h(App),
+}).$mount('#app')
